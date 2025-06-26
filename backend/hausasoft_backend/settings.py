031989286d813da6679ab256f76415812555568d
Abubakar Abdulrahim <abubakarabdulrahimibrahim@gmail.com>
2025-06-26 12:45:03 +0100

    fix connections

--- backend/hausasoft_backend/settings.py
+++ backend/hausasoft_backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,').split(',')
 
 # APPLICATIONS
 INSTALLED_APPS = [
